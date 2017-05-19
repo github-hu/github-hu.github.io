30e6bba2e3e2df9667be134b5b8a09ae96fc0032
github-hu <18039615513@163.com>
2017-05-19 21:45:44 +0800

    实现主页第一行js效果，主要精灵兔定位，和鼠标拉动实现下拉slide

--- js/js/main.js
+++ js/js/main.js
@@ -1,9 +1,61 @@
 define(['jquery'],function(){
 		$(function(){
-		var mygou =$('#my_gou');
-		mygou.on('mouseenter',function(){
-			console.log(this);
-		})
+		var slide =$('.fr-slide');
+		
+		slide.on('mouseenter',function(){
+			$(this).find('.slide_gou')
+			.css({
+				'border-color': '#ccc',
+			}).stop().slideDown(300)
+			.end().find('.phone')
+			.css({
+				'background-position':'-44px 0'
+			})
+			.end().find('.homeicon')
+			.css({
+				'background-position':'-69px 0'
+			})
+			.end()
+			.css({
+				background:"#fff",
+				'border-color': '#ccc',	
+			})
+			
+			
+			
+		});
+		
+		slide.on('mouseleave',function(){
+			$(this).find('.slide_gou')
+			.css({
+				'border-color': '#fff',
+			}).stop().slideUp(200)
+			.end().find('.phone')
+			.css({
+				'background-position':'-24px 0'
+			})
+			.end().find('.homeicon')
+			.css({
+				'background-position':'-89px 0'
+			})
+			.end()
+			.css({
+				background:"#f5f5f5",
+				'border-color': '#f5f5f5',
+			});
+			
+		});
+		
+		$('.fr-icon .icoll').on('mouseenter',function(){
+			$(this).css({
+				'background-position':'-41px -21px'
+			})
+		});
+		$('.fr-icon .icoll').on('mouseleave',function(){
+			$(this).css({
+				'background-position':'-64px -21px'
+			})
+		});
 	})
 })
 
